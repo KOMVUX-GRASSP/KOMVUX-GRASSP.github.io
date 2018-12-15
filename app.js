566ff61d5f291d7366c39157c678ff02e4974233
Pratibha S <prati1410@gmail.com>
2018-12-15 16:09:56 +0100

    first merge

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
-var app = angular.module('appToilet',['ngRoute']);
+   
+var app = angular.module("GRASSPApp",['ngRoute','firebase']);
+var cafeHelsingborg = angular.module("cafeApp",['ngRoute']);
 
-app.config(function($RouteProvider,$locationProvider){
-    
-})
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templatesUrl:'templates/homess.html',
+        controller:'FirstController',  
+    })
+    .when('/sign-up',{
+        templatesUrl:'templates/sign-up.html',
+        controller:'SignUpController'
+    })
+    .when('/login',{
+        templatesUrl:'templates/login.html',
+        controller:'LogInController'
+    }) 
+});
+
+cafeHelsingborg.config(function($routeProvider){
+    $routeProvider
+    .when('/cafe',{
+        templatesUrl:'/cafe.html',
+        controller:'cafeSearchCtrl'
+    }) 
+    .when('/cafes',{
+        templatesUrl:'templates/cafes.html',
+        controller:'cafeServiceCtrl'
+    })
+    .when('/cafe-map/:coordinate_noBracket', {
+        templatesUrl: 'templates/cafeSearch-map.html',
+        controller: 'cafeSearchMapCtrl'
+    }) 
+    .when('/restaurants_select', {
+        templatesUrl: 'templates/restaurants_select.html',
+        controller: 'restaurantServiceCtrl'
+    }) 
+    .when('/restaurant-map/:coordinate_noBracket', {
+        templatesUrl:'templates/restaurant_search_map.html',
+        controller: 'restaurantMapCtrl'
+    });
+});
+
+
+
+
+
+
+
+
